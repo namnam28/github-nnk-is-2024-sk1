6b2f099c6635ffbe73415ab85708c02f0a712923
Ngo Nam Khánh <nam.khanh.ngo@tul.cz>
2024-10-09 09:45:19 +0200

    bude potřeba změna

--- IS-Projekty/001-prvni-program-vypis-rady/Program.cs
+++ IS-Projekty/001-prvni-program-vypis-rady/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+static void Main() {
+        
+    string again = "a";
+    
+        
+    while(again=="a") {}
+        Console.Clear();
+        Console.WriteLine("*************************");
+        Console.WriteLine("*****Výpis rady čísel****");
+        Console.WriteLine("*************************");
+        Console.WriteLine("******Ngo Nam Khánh******");
+        Console.WriteLine("*************************");
+        Console.WriteLine();
+
+        Console.WriteLine();
+        Console.WriteLine("Pro opakování programu stisknutí klávesy A");
+        again = Console.ReadLine();
+
+
+
+       
+    } 
+}
+
+
+
+
+
+
+// toto je jednořádkový komentář 
+
+/* toto je
+více řadkový
+komentář */
+
+
+
+
+
+

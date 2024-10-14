591d77cedce8b9b67c5da1e7b374b6db9ef75d1c
Ngo Nam Khánh <nam.khanh.ngo@tul.cz>
2024-10-14 15:35:06 +0200

    what

--- IS-Projekty/001-prvni-program-vypis-rady/Program.cs
+++ IS-Projekty/001-prvni-program-vypis-rady/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using System.Data;
+class Program {
+    static void Main() {
+
+        string again =  "a";
+
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("*****Výpis řady čísel*****");
+            Console.WriteLine("**************************");
+            Console.WriteLine("******Ngo Nam Khánh*******");
+            Console.WriteLine("**************************");
+            Console.WriteLine();
+
+            // Vstup hodnot do programu - špatně řešený
+            // Console.Write("Zadejte první číslo řady: ");
+            // int First = int.Parse(Console.ReadLine());
+
+            // Vstup hodnot do programu - správně řešený
+
+        }
+    }
+}

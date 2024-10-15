52e6e1340cc9df9ef3b262411412d22c8df3b6ef
Ngo Nam Khánh <nam.khanh.ngo@tul.cz>
2024-10-15 19:25:42 +0200

    maybeagain

--- IS-Projekty/001-prvni-program-vypis-rady/Program.cs
+++ IS-Projekty/001-prvni-program-vypis-rady/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        string again = "a";
+
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("***********************");
+            Console.WriteLine("***Výpis řady čísel****");
+            Console.WriteLine("***********************");
+            Console.WriteLine("*****Ngo Nam Khánh*****");
+            Console.WriteLine("***********************");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }
+}
+

b3b29566136a0480e5be4e281be4927331e8ed66
Ngo Nam Khánh <nam.khanh.ngo@tul.cz>
2024-10-16 09:20:22 +0200

    novy program

---
+++ IS-Projekty/002-druhy-program-soucet-cifer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+
+class Program {
+
+    static void Main() {
+
+        string again = "a";
+
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("***********************");
+            Console.WriteLine("***Výpis řady čísel****");
+            Console.WriteLine("***********************");
+            Console.WriteLine("*****Ngo Nam Khánh*****");
+            Console.WriteLine("***********************");
+            Console.WriteLine();
+
+            Console.Write("Zadejte celé číslo ");
+            int number;
+            while(!int.TryParse(Console.ReadLine(), out number)) {
+                Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu" );
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+
+         }
+     }
+ }
+
+
+
+

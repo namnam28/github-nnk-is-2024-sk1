bf77dfe9db6e5af23c4042baaf1b57dfb63c3556
Ngo Nam Khánh <nam.khanh.ngo@tul.cz>
2024-10-16 09:40:26 +0200

    l

--- IS-Projekty/002-druhy-program-soucet-cifer/Program.cs
+++ IS-Projekty/002-druhy-program-soucet-cifer/Program.cs
@@ -22,6 +22,24 @@ class Program {
                 Console.WriteLine("Nezadali jste celé číslo. Zadejte znovu" );
             }
             
+            int suma = 0;
+            int numberBackup = number;
+            int digit;
+
+            while(number >= 10) {
+                digit = number % 10;
+                number = (number-digit)/10;
+                Console.WriteLine("Digit = {0}", digit);
+                suma = suma + digit;
+            }
+            Console.WriteLine("Digit = {0}", number);
+
+            // musíme přičíst poslední cyklus
+            suma = suma + number;
+
+            Console.WriteLine();
+            Console.WriteLine("\n\n\nSoučet cifer čísla {0} je {1}", numberBackup, suma,);
+
             Console.WriteLine();
             Console.WriteLine("Pro opakování programu stiskněte klávesu a");
             again = Console.ReadLine();

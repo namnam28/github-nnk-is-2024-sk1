3c1062f56b72901a0b9d2600834b0e431663d35a
Ngo Nam Khánh <nam.khanh.ngo@tul.cz>
2024-10-16 09:03:08 +0200

    2

--- IS-Projekty/001-prvni-program-vypis-rady/Program.cs
+++ IS-Projekty/001-prvni-program-vypis-rady/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 class Program {
     static void Main() {
@@ -13,6 +14,36 @@ class Program {
             Console.WriteLine("*****Ngo Nam Khánh*****");
             Console.WriteLine("***********************");
             Console.WriteLine();
+
+
+
+            Console.Write("Zadejte první číslo (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+            }
+
+             Console.Write("Zadejte poslední číslo (celé číslo): ");
+            int last;
+            while(!int.TryParse(Console.ReadLine(), out last)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+            }
+
+
+            Console.Write("Zadejte diferenci (celé číslo): ");
+            int step;
+            while(!int.TryParse(Console.ReadLine(), out step)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+            }
+
+           Console.WriteLine();
+           Console.WriteLine("==================");
+           Console.WriteLine("Výpis číselné řady");
+           int current = first;
+           while(current <= last) {
+                Console.WriteLine(current);
+                current = current + step;
+           }
 
             Console.WriteLine();
             Console.WriteLine("Pro opakování programu stiskněte klávesu a");

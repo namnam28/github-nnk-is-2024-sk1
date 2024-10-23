b3f6d5ca428bae34f351e4cc8465276302b0c30a
Ngo Nam Khánh <nam.khanh.ngo@tul.cz>
2024-10-23 07:47:24 +0200

    ???

--- IS-Projekty/001-prvni-program-vypis-rady/obj/Debug/net8.0/001-prvni-program-vypis-rady.AssemblyInfo.cs
+++ IS-Projekty/001-prvni-program-vypis-rady/obj/Debug/net8.0/001-prvni-program-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("001-prvni-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29c9107754b3dc5a9468a33f9e488334facc589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caf81de4309dbf855ed9e12dfae0cf430a950fca")]
 [assembly: System.Reflection.AssemblyProductAttribute("001-prvni-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("001-prvni-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

0a54471cc316f8bb0387148402c9ad0329054708
Ngo Nam Khánh <nam.khanh.ngo@tul.cz>
2024-10-23 08:00:39 +0200

    ok?

--- IS-Projekty/003b-trojuhelnik/Program.cs
+++ IS-Projekty/003b-trojuhelnik/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string again = "a";
+while(again == "a") {
+
+    Console.Clear();
+    Console.WriteLine("***********************"); 
+    Console.WriteLine("****Trojúhelník******"); 
+    Console.WriteLine("***********************"); 
+    Console.WriteLine("*****Ngo Nam Khánh*******"); 
+    Console.WriteLine();
+    Console.WriteLine();
+
+    Console.Write("Zadejte číslo (celé číslo): ");
+    uint a;
+    while(!uint.TryParse(Console.ReadLine(), out a)) {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+    }
+
+
+    Console.WriteLine();
+    Console.WriteLine("============================="); 
+    Console.WriteLine("Zadané hodnoty: ");
+    Console.WriteLine("Šířka: {0}", a);
+    Console.WriteLine("=============================");
+
+    for(int y = 1; y < a ;y++){
+        for(int x = 1; x < y ;x++){
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("Program můžete opakovat stskem klávesy a");
+    again = Console.ReadLine();
+}
+
+

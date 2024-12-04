60b3b7fdf20fdd6a9cb0e1d1af389400dd6a608d
Ngo Nam Khánh <nam.khanh.ngo@tul.cz>
2024-12-04 09:03:25 +0100

    nsn

--- IS-Projekty/001-prvni-program-vypis-rady/obj/Debug/net8.0/001-prvni-program-vypis-rady.AssemblyInfo.cs
+++ IS-Projekty/001-prvni-program-vypis-rady/obj/Debug/net8.0/001-prvni-program-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("001-prvni-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a5b92dabea57ecbc316b575a3e79c568f90838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20e558c0bba041b24bc100a42c8a85a04c5010b")]
 [assembly: System.Reflection.AssemblyProductAttribute("001-prvni-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("001-prvni-program-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- IS-Projekty/012-generator2/Program.cs
+++ IS-Projekty/012-generator2/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Authentication.ExtendedProtection;
+
+string again = "a";
+
+    while(again == "a") {
+    Console.Clear();
+    Console.WriteLine("***********************"); 
+    Console.WriteLine("******Generátor čísel******"); 
+    Console.WriteLine("***********************"); 
+    Console.WriteLine("*****Ngo Nam Khánh*****"); 
+    Console.WriteLine();
+    Console.WriteLine();
+
+    Console.Write("Zadejte počet generovaných čísel (celé číslo): ");
+    int n = 10; 
+    while(!int.TryParse(Console.ReadLine(), out n)) {
+        Console.Write("Nezadali jste celé číslo. Zadejte počet generovaných číslo znovu: ");
+    }
+
+    Console.Write("Zadejte dolní mez (celé číslo): ");
+    int dm = 0;
+    while(!int.TryParse(Console.ReadLine(), out dm)) {
+        Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu: ");
+    }
+
+    Console.Write("Zadejte horní (celé číslo): ");
+    int hm = 100;
+    while(!int.TryParse(Console.ReadLine(), out hm)) {
+        Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu: ");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("============================================");
+    Console.WriteLine("Zadané hodnoty: ");
+    Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez: {2}", n, dm, hm);
+    Console.WriteLine("============================================");
+    Console.WriteLine();
+
+
+
+
+
+        // deklarace pole
+        int[] myArray = new int[n];
+
+        Random randomNumber = new Random();
+
+        int kladna = 0;
+        int zaporna = 0;
+        int nuly = 0;
+
+        Console.WriteLine("\n\nNáhodná čísla:");
+        for(int i=0; i<n; i++) {
+            myArray[i] = randomNumber.Next(dm, hm+1);
+            Console.Write("{0}; ", myArray[i]);
+
+            if(myArray[i]>0)
+                kladna++;
+            else if(myArray[i]<0)
+                zaporna++;
+            else
+                nuly++;
+        }
+    
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+
+//if(myArray[i]<= (0.25 * hm){
+// }
+//)
